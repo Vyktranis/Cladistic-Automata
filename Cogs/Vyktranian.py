ebf9f50014a48aac4ce7c485d63784c9f21b1ee0
WhyDoWeLiveWithoutMeaning <68759343+WhyDoWeLiveWithoutMeaning@users.noreply.github.com>
2021-08-10 08:49:41 -0400

    Add Stuff

---
+++ Cogs/Vyktranian.py
@@ -0,0 +1,23 @@
+import typing
+import asyncio
+import discord
+from discord.ext import commands
+
+class Vyktranian:
+    """Vykdom Member
+
+    This represents a Vykdom Member
+
+    parms:
+        data -- Discord Member or Database data
+    """
+
+    def __init__(self, data):
+        pass
+
+    @classmethod
+    async def convert(ctx, argument):
+        member = commands.MemberConverter.convert(ctx, argument)
+        ## DB to get member data
+        #return cls()
+        pass
