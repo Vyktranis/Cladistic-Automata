3ee1932f71365ecae32552fc7182dc52641e3b30
WhyDoWeLiveWithoutMeaning <68759343+WhyDoWeLiveWithoutMeaning@users.noreply.github.com>
2021-08-10 21:50:05 -0400

    for Lach to add his code from Audit-Bot into

---
+++ Cogs/Audit.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+class Audit(commands.Cog):
+    
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener
+    async def on_message_delete(self, message):
+        pass
+
+    @commands.Cog.listener
+    async def on_message_edit(self, before, after):
+        pass
+
+    @commands.Cog.listener
+    async def on_member_update(self, before, after):
+        pass
+
+    @commands.Cog.listener
+    async def on_member_join(member):
+        pass
+
+    @commands.Cog.listener
+    async def on_member_remove(member):
+        pass
