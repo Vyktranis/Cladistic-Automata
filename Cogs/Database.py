116b89ffeaf05c015dc80881390fbb8447436304
WhyDoWeLiveWithoutMeaning <68759343+WhyDoWeLiveWithoutMeaning@users.noreply.github.com>
2021-08-12 12:53:09 -0400

    Made it a bit cuter :3

--- Cogs/Database.py
+++ Cogs/Database.py
@@ -3,8 +3,7 @@ import requests
 import secrets
 
 from discord.ext import commands
-from random_word import RandomWords
-from Functions import Roblox, DB
+from Functions import Roblox, DB, Cuter
 from Objects import Errors
 
 class Database(commands.Cog):
@@ -15,49 +14,10 @@ class Database(commands.Cog):
     @commands.command(aliases=["reverify"])
     async def verify(self, ctx):
 
-        await ctx.reply("Verification Process in DM's")
-        first_message = await ctx.author.send("What is your Roblox Username? (Case Sensitive)")
+        user, verified = Cuter.verify_user(ctx, self.client)
 
-        def checkMessage(message):
-            return message.channel == first_message.channel and message.author == ctx.author
-
-        def checkReaction(reaction, user):
-            return reaction.message.channel == first_message.channel and user == ctx.author and reaction.emoji in ['✅', '❌']
-
-        name_message = await self.client.wait_for('message', check=checkMessage)
-
-        try:
-            roblox_user = Roblox.getUserFromName(name_message.content)
-        except Errors.RobloxNamesNotFound or Errors.RobloxUserNotFound:
-            await ctx.send("Could not find any User.\nTry again and make sure its case sensitive.")
-            return
-
-        checking = await ctx.author.send(
-            content="Is this you?",
-            embed=roblox_user.embed().set_author(name="React with ✅ If it is else ❌")
-        )
-
-        await checking.add_reaction("✅")
-        await checking.add_reaction("❌")
-
-        reac, usr = await self.client.wait_for('reaction_add', check=checkReaction)
-
-        if reac.emoji == '❌':
-            await ctx.author.send("Try again.")
-            return
-
-        async with ctx.author.typing():
-            code = " ".join([RandomWords().get_random_word() for _ in range(5)])
-            verification_message = await ctx.author.send(f"Set this code to your About: {code}\n\nReact when complete!")
-
-        await verification_message.add_reaction("✅")
-
-        reac, usr = await self.client.wait_for('reaction_add', check=checkReaction)
-
-        roblox_user2 = Roblox.getUserFromID(roblox_user.id)
-
-        if code in roblox_user2.description:
-            DB.verify_account(ctx.author.id, roblox_user2)
+        if verified:
+            DB.verify_account(ctx.author.id, user)
             await ctx.author.send("Account Linked")
         else:
             await ctx.author.send("Failed, Please try again.")
---
+++ Functions/Cuter.py
@@ -0,0 +1,58 @@
+"""
+Vykdom Bot Cuter Functions
+~~~~~~~~~~~~~~~~~~~
+
+Functions that are too ugly will be put here for the Vykdom Discord Bot.
+
+:copyright: (c) 2021-present Meaning
+:license: GNU General Public License v3.0, see LICENSE for more details.
+"""
+
+from random_word import RandomWords
+from Functions import Roblox, DB
+from Objects import Errors
+
+## Cogs.Database.py
+
+async def verify_user(ctx, client):
+    await ctx.reply("Verification Process in DM's")
+    first_message = await ctx.author.send("What is your Roblox Username? (Case Sensitive)")
+
+    def checkMessage(message):
+        return message.channel == first_message.channel and message.author == ctx.author
+
+    def checkReaction(reaction, user):
+        return reaction.message.channel == first_message.channel and user == ctx.author and reaction.emoji in ['✅', '❌']
+
+    name_message = await client.wait_for('message', check=checkMessage)
+
+    try:
+        roblox_user = Roblox.getUserFromName(name_message.content)
+    except Errors.RobloxNamesNotFound or Errors.RobloxUserNotFound:
+        await ctx.send("Could not find any User.\nTry again and make sure its case sensitive.")
+        return
+
+    checking = await ctx.author.send(
+        content="Is this you?",
+        embed=roblox_user.embed().set_author(name="React with ✅ If it is else ❌")
+    )
+
+    await checking.add_reaction("✅")
+    await checking.add_reaction("❌")
+
+    reac, usr = await client.wait_for('reaction_add', check=checkReaction)
+
+    if reac.emoji == '❌':
+        await ctx.author.send("Try again.")
+        return
+
+    async with ctx.author.typing():
+        code = " ".join([RandomWords().get_random_word() for _ in range(5)])
+        verification_message = await ctx.author.send(f"Set this code to your About: {code}\n\nReact when complete!")
+        await verification_message.add_reaction("✅")
+
+    reac, usr = await client.wait_for('reaction_add', check=checkReaction)
+
+    roblox_user2 = Roblox.getUserFromID(roblox_user.id)
+
+    return (roblox_user2, code in roblox_user2.description)
